ef582b1baa2a9da1959d107fe9670f555defb1bd
7zYBN <frazo4ka@mail.ru>
2019-10-19 19:28:16 +0300

    feat: add completed task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.binaryMin = null;
+        this.binaryMax = null;
+        this.result = null;
+    }
 
     setRange(min, max) {
-
+        this.binaryMin = min;
+        this.binaryMax = max;
     }
 
     guess() {
-
+        this.result = Math.ceil((this.binaryMin + this.binaryMax) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.setRange(this.binaryMin, this.result);
     }
 
     greater() {
-
+        this.setRange(this.result, this.binaryMax);
     }
 }
 
